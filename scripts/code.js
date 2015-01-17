183a0c3019d444b4d390f74093447c186ca1cebd
Aditya Keerthi <pxlcoder@gmail.com>
2015-01-17 18:48:35 -0500

    Content Added

---
+++ scripts/code.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+	$(window).resize(function(){
+		var vHeight = $(window).height();
+		var topPadding = (vHeight)/(2);
+		var bottomPadding = (topPadding)/(1.54);
+		
+		$('header > .container').css({"padding-top":topPadding, "padding-bottom":bottomPadding});
+		
+		var scrollOffset = 0;
+		
+		if($(window).width() < 768){
+			scrollOffset = 70;
+		}else{
+			scrollOffset = 90;
+		}
+		
+		$('.navbar').singlePageNav({
+			offset: scrollOffset
+		});
+		
+	});
+	
+	$(window).resize();
+	
+	// Close Menu on Click
+	$('.navbar li a').click(function(e) {
+       $('.navbar-toggle:visible').click();
+    });
+	
+	// Highlight Links
+	$('body').scrollspy({ 
+		target: '.navbar',
+		offset: 200
+	});
+});

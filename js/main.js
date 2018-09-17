b77fb7f2b6f2a1705ac58bc9cf6573114c63c065
Aditya Keerthi <pxlcoder@gmail.com>
2018-09-16 23:12:48 -0400

    Redesign site and update resume

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".navigation-item a").click(function(event) {
-		//console.log(event.target.href);
-
-    	$('html, body').animate({
-        	scrollTop: $(event.target.id).offset().top
-    	}, 1000);
-	});
-});
